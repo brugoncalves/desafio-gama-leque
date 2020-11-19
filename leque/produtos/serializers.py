ffe571e3d5e8b31adb37ffc3a666d5aa6319e648
Valeria Cintra <vacbarci@hotmail.com>
2020-11-19 20:39:00 -0300

    Adicionado Hateoas

--- leque/produtos/serializers.py
+++ leque/produtos/serializers.py
@@ -2,13 +2,14 @@ from rest_framework import serializers
 from .models import Seller, Product
 
 
-class SellerSerializer(serializers.ModelSerializer):
+class SellerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Seller
-        fields = ['id', 'name', 'email']
+        fields = ['id', 'name', 'email', 'url']
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'name', 'price', 'seller', 'quantity', 'status']
+        fields = ['id', 'name', 'price', 'seller', 'quantity', 'status', 'url']
+
