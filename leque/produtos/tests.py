76deb2c17a1658c9b443789162dac1e29086f040
Kelly Lobato <kcarladl@gmail.com>
2020-11-19 21:28:42 -0300

    Adicionado teste da Classe Seller

--- leque/produtos/tests.py
+++ leque/produtos/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Seller
 
-# Create your tests here.
+class SellerModelTests(TestCase):
+    
+    def test_class_str(self):
+        seller=Seller()
+        seller.name="Bruna"
+    
+        self.assertEquals(seller.__str__(),"Bruna")
+
+    def test_to_dict(self):
+        seller=Seller()
+        seller.name="Bruna"
+        seller.email="bruna@example.com"
+
+        result={
+            "id":None,
+            "name":"Bruna",
+            "email":"bruna@example.com"
+        }
+
+        self.assertEquals(seller.to_dict(),result)
+
+    def test_class_str_without_name(self):
+        seller=Seller()
+        self.assertEqual(seller.__str__(),"")
