fc34b83e8c665f1849323ac4688493f75a314656
Brenda <brendapires.ps@gmail.com>
2020-11-20 22:25:19 -0300

    Adicionado teste de integração da rota dos métodos GET e POST

--- leque/leque/urls.py
+++ leque/leque/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('produtos/', include('produtos.urls'))
-
+    path('produtos/', include('produtos.urls')),
+    path('', lambda request: redirect('produtos/', permanent=False))
 ]
--- leque/produtos/tests.py
+++ leque/produtos/tests.py
@@ -3,30 +3,31 @@ from .models import Seller, Product
 from rest_framework.test import APIClient
 import json
 
+
 class SellerModelTests(TestCase):
-    
+
     def test_class_str(self):
         seller = Seller()
         seller.name = "Bruna"
-    
-        self.assertEquals(seller.__str__(),"Bruna")
+
+        self.assertEquals(seller.__str__(), "Bruna")
 
     def test_to_dict(self):
-        seller=Seller()
-        seller.name="Bruna"
-        seller.email="bruna@example.com"
+        seller = Seller()
+        seller.name = "Bruna"
+        seller.email = "bruna@example.com"
 
-        result_seller={
-            "id":None,
-            "name":"Bruna",
-            "email":"bruna@example.com"
+        result_seller = {
+            "id": None,
+            "name": "Bruna",
+            "email": "bruna@example.com"
         }
 
-        self.assertEquals(seller.to_dict(),result_seller)
+        self.assertEquals(seller.to_dict(), result_seller)
 
     def test_class_str_without_name(self):
-        seller=Seller()
-        self.assertEqual(seller.__str__(),"")
+        seller = Seller()
+        self.assertEqual(seller.__str__(), "")
 
 
 class ProductModelTests(TestCase):
@@ -37,7 +38,6 @@ class ProductModelTests(TestCase):
 
         self.assertEquals(product.__str__(), "Cadeira")
 
-
     def product_to_dict(self):
         product = Product()
         product.name = "Cadeira"
@@ -46,21 +46,19 @@ class ProductModelTests(TestCase):
         product.quantity = 10
         product.status = "Active"
 
-
-        result_product= {
+        result_product = {
             "id": None,
             "name": "Cadeira",
-            "price":2000,
-            "seller":"Bruna",
-            "quantity":10,
-            "status": "Active"        
-        
-        }
+            "price": 2000,
+            "seller": "Bruna",
+            "quantity": 10,
+            "status": "Active"
 
+        }
 
         self.assertEquals(product.to_dict(), result_product)
 
-        
+
 class SellerViewTests(TestCase):
     @classmethod
     def setUpTestData(cls):
@@ -80,3 +78,49 @@ class SellerViewTests(TestCase):
 
         self.assertEqual(seller_first.get("name"), "Bruna")
         self.assertEqual(seller_first.get("email"), "bruna@example.com")
+
+    def test_post(self):
+        client = APIClient()
+        response = client.post('/produtos/sellers/', {
+            "name": "Bruna",
+            "email": "bruna@example.com"
+        })
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEquals(Seller.objects.count(), 2)
+        self.assertEquals(Seller.objects.last().name, "Bruna")
+
+
+class ProductViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Product.objects.create(name="Cadeira", price=250, quantity=2, status="Active")
+
+    def test_get(self):
+        client = APIClient()
+        response = client.get('/produtos/')
+
+        self.assertEqual(response.status_code, 200)
+
+        data = json.loads(response.content)
+        self.assertEqual(data.get('count'), 1)
+
+        product_first = data.get('results')[0]
+
+        self.assertEqual(product_first.get("name"), "Cadeira")
+        self.assertEqual(product_first.get("price"), 250)
+        self.assertEqual(product_first.get("quantity"), 2)
+        self.assertEqual(product_first.get("status"), "Active")
+
+    def test_post(self):
+        client = APIClient()
+        response = client.post('/produtos/', {
+            "name": "Mesa",
+            "price": 300,
+            "quantity": 2,
+            "status": "ACTIVE"
+        })
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEquals(Product.objects.count(), 2)
+        self.assertEquals(Product.objects.last().name, "Mesa")
